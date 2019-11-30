89cbd036282fe2a4bb6b045507afc0e017f744bc
Simon Gonzalez Negrete <simongonzaleznegrete90@gmail.com>
2019-11-30 12:54:50 +0100

    Deploy to gh-pages

---
+++ out/_next/static/webpack/static/development/pages/index.js.4b0c51a716294d7c4e8e.hot-update.js
@@ -0,0 +1,117 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Project/Project.js":
+/*!***************************************!*\
+  !*** ./components/Project/Project.js ***!
+  \***************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _Button_Button__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../Button/Button */ "./components/Button/Button.js");
+/* harmony import */ var _ProjectStyle__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./ProjectStyle */ "./components/Project/ProjectStyle.js");
+/* harmony import */ var _styles_fontsStyles_fontP__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../styles/fontsStyles/fontP */ "./styles/fontsStyles/fontP.js");
+var _jsxFileName = "/Users/simongnegrete/Desktop/Tailor/Portfolio/components/Project/Project.js";
+
+
+
+
+
+
+var Project = function Project(_ref) {
+  var img = _ref.img,
+      client = _ref.client,
+      stack = _ref.stack,
+      links = _ref.links;
+
+  var displayLinks = function displayLinks() {
+    return links.map(function (link, i) {
+      var name = link.name,
+          url = link.url;
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+        rel: "nofollow noopener noreferrer",
+        href: url,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 14
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 14
+        },
+        __self: this
+      }, name));
+    });
+  };
+
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_ProjectStyle__WEBPACK_IMPORTED_MODULE_3__["ProjectStyle"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: img,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "img",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "info",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 24
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 25
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles_fontsStyles_fontP__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 26
+    },
+    __self: this
+  }, " ", client), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles_fontsStyles_fontP__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    align: "right",
+    size: "15px",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 27
+    },
+    __self: this
+  }, " ", stack)), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "button",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29
+    },
+    __self: this
+  }, displayLinks())));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Project);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.4b0c51a716294d7c4e8e.hot-update.js.map
